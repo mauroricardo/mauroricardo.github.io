90d20c893ab1da7ef02807b7b9d3bc69db4b555d
mauroricardo <146049532+mauroricardo@users.noreply.github.com>
2026-03-14 17:05:27 -0500

    Crear services/java/ToolsAndGamesMS/ToolsAndGamesApplication.java

---
+++ services/java/ToolsAndGamesMS/ToolsAndGamesApplication.java
@@ -0,0 +1,8 @@
+@SpringBootApplication
+public class ToolsAndGamesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InventoryApplication.class, args);
+    }
+
+}
