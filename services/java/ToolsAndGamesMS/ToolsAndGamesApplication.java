cf072668daeab6a2156f728d77bae4d504b9effb
mauroricardo <146049532+mauroricardo@users.noreply.github.com>
2026-03-15 19:24:37 -0500

    Delete services/java/ToolsAndGamesMS/ToolsAndGamesApplication.java

--- services/java/ToolsAndGamesMS/ToolsAndGamesApplication.java
+++
@@ -1,8 +0,0 @@
-@SpringBootApplication
-public class ToolsAndGamesApplication {
-
-    public static void main(String[] args) {
-        SpringApplication.run(InventoryApplication.class, args);
-    }
-
-}
