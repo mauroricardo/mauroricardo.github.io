a783336b7bbf6e2ff71de96a8b35c3491a7f97df
mauroricardo <146049532+mauroricardo@users.noreply.github.com>
2026-03-15 19:26:39 -0500

    Add files via upload

---
+++ ‎services/java/ToolsAndGamesMS/src/main/java/com/toolsandgames/ToolsAndGamesApplication.java
@@ -0,0 +1,13 @@
+package com.toolsandgames;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToolsAndGamesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MrmToolsAndGamesApplication.class, args);
+	}
+
+}
